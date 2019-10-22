5a07a0aff3a2d505968f0aa1ed00f29a45d602bd
Adam Weissman <adam@comedytragedyepic.com>
2019-10-22 09:28:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,10 @@ class Student
       self.send("#{attribute}=", value)
     end
     @@all << self
+    
+    #   student_hash.each {|key, value| self.send(("#{key}="), value)}
+    #@@all << self
+    
   end
 
   def self.create_from_collection(students_array)
