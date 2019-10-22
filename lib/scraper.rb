2ce71be80c90b2470b8ce168e79e0a4262f93e50
Adam Weissman <adam@comedytragedyepic.com>
2019-10-22 09:19:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,8 +23,30 @@ def self.scrape_index_page(index_url)
   
   
 
-  def self.scrape_profile_page(profile_url)
-    
+    def self.scrape_profile_page(profile_url)
+    html = File.read(profile_url)
+    profile = Nokogiri::HTML(html)
+    student = {}
+
+    links = profile.css(".social-icon-container a").map do |link|
+      link.attribute("href").value
+    end
+
+    links.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?(".com")
+        student[:blog] = link
+      end
+    end
+
+    student[:profile_quote] = profile.css(".profile-quote").text
+    student[:bio] = profile.css(".description-holder p").text
+    student
   end
 
 end
