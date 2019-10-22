3edbc011a62f59b8b495e3d9788858f95394a26f
Adam Weissman <adam@comedytragedyepic.com>
2019-10-22 09:20:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,7 @@ require 'pry'
 
 class Scraper
 
-def self.scrape_index_page(index_url)
-
+  def self.scrape_index_page(index_url)
     #for the student name = doc.css(".student-name").text
     #location = doc.css(".student-location").text
     #website = student.css("a").attribute("href").value
